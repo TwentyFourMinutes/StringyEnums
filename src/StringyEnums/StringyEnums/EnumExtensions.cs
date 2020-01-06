ab9a9d755d8a8357cd66ecc0a1e1fbd97294fcdd
Twenty <twenty@translucent.at>
2020-01-06 01:14:09 +0100

    Added Try implementation to all methods

--- src/StringyEnums/StringyEnums/EnumExtensions.cs
+++ src/StringyEnums/StringyEnums/EnumExtensions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace StringyEnums
@@ -6,25 +6,47 @@ namespace StringyEnums
 	public static class EnumExtensions
 	{
 		public static string GetRepresentation<TEnum>(this TEnum enumValue) where TEnum : struct, Enum
+			=> EnumCore.RepresentationCache[typeof(TEnum)][(int)(dynamic)enumValue];
+
+		public static bool TryGetRepresentation<TEnum>(this TEnum enumValue, out string? representation) where TEnum : struct, Enum
 		{
 			if (EnumCore.RepresentationCache.TryGetValue(typeof(TEnum), out var dict))
 			{
-				if (dict.TryGet((int)(dynamic)enumValue, out var representation))
-				{
-					return representation;
-				}
+				return dict.TryGet((int)(dynamic)enumValue, out representation);
 			}
 
-			throw new InvalidOperationException("The enum passed, isn't in any assemblies passed to the EnumCore.Init method.");
+			representation = default;
+			return false;
 		}
 
 		public static string[] GetFlagRepresentation<TEnum>(this TEnum enumValue) where TEnum : struct, Enum
+		{
+			var dict = EnumCore.RepresentationCache[typeof(TEnum)];
+
+			var flags = (int)(dynamic)enumValue;
+
+			var tempArr = new string[GetHammingWeight(flags)];
+
+			int lastIndex = 0;
+
+			for (int i = 1; i <= flags; i *= 2)
+			{
+				if ((flags & i) != 0)
+				{
+					tempArr[lastIndex++] = dict[i];
+				}
+			}
+
+			return tempArr;
+		}
+
+		public static bool TryGetFlagRepresentation<TEnum>(this TEnum enumValue, out string[]? representations) where TEnum : struct, Enum
 		{
 			if (EnumCore.RepresentationCache.TryGetValue(typeof(TEnum), out var dict))
 			{
 				var flags = (int)(dynamic)enumValue;
 
-				var tempArr = new string[GetHammingWeight(flags)];
+				representations = new string[GetHammingWeight(flags)];
 
 				int lastIndex = 0;
 
@@ -32,30 +54,41 @@ namespace StringyEnums
 				{
 					if ((flags & i) != 0)
 					{
-						tempArr[lastIndex++] = dict[i];
+						if (dict.TryGet(i, out var representation))
+						{
+							representations[lastIndex++] = representation;
+						}
+						else
+						{
+							return false;
+						}
 					}
 				}
-				
-				return tempArr;
+
+				return true;
 			}
-			else
-			{
-				throw new InvalidOperationException("The enum passed, isn't in the assembly passed to the EnumCore.InitWith method.");
-			}
+
+			representations = default;
+			return false;
 		}
 
 		public static TEnum GetEnumFromRepresentation<TEnum>(this string val) where TEnum : struct, Enum
+			=> (TEnum)(dynamic)EnumCore.RepresentationCache[typeof(TEnum)][val];
+
+		public static bool TryGetEnumFromRepresentation<TEnum>(this string val, out TEnum enumResult) where TEnum : struct, Enum
 		{
 			if (EnumCore.RepresentationCache.TryGetValue(typeof(TEnum), out var dict))
 			{
-				dict.TryGet(val, out var intVal);
+				if (dict.TryGet(val, out var intVal))
+				{
+					enumResult = (TEnum)(dynamic)intVal;
 
-				return (TEnum)(dynamic)(intVal);
+					return true;
+				}
 			}
-			else
-			{
-				throw new InvalidOperationException($"Couldn't parse '{val}' to {nameof(TEnum)} the enum wasn't in any assemblies passed to the EnumCore.InitWith method.");
-			}
+
+			enumResult = default;
+			return false;
 		}
 
 		private static int GetHammingWeight(int value)
@@ -63,6 +96,6 @@ namespace StringyEnums
 			value -= ((value >> 1) & 0x55555555);
 			value = (value & 0x33333333) + ((value >> 2) & 0x33333333);
 			return (((value + (value >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
-		}
+		} 
 	}
 }
