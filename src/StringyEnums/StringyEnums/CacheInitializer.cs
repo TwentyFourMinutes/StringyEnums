413c90d8b1d8f3075293e362b8ef53cd6059594a
Twenty <twenty@translucent.at>
2020-01-06 02:52:30 +0100

    Added more flexible initializing options for the CacheInitializer

--- src/StringyEnums/StringyEnums/CacheInitializer.cs
+++ src/StringyEnums/StringyEnums/CacheInitializer.cs
@@ -1,4 +1,4 @@
-using BidirectionalDict;
+﻿using BidirectionalDict;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -31,33 +31,42 @@ namespace StringyEnums
 
 				foreach (var enumType in enumTypes)
 				{
-					var fields = enumType.GetFields();
-
-					var temptDict = new BiDictionary<int, string>();
-
-					foreach (var field in fields.Where(x => x.IsStatic))
-					{
-						var attr = field.GetCustomAttribute<StringRepresentationAttribute>();
-
-						if (attr is { })
-						{
-							temptDict.TryAdd((int)field.GetValue(enumType)!, attr.StringRepresentation);
-						}
-					}
-
-					if (temptDict.Count > 0)
-					{
-						_tempCache.Add(enumType, temptDict);
-					}
+					InitWith(enumType);
 				}
 			}
 
 			return this;
 		}
 
+		/// <summary>
+		/// Initializes the cache with a given enum.
+		/// </summary>
+		/// <typeparam name="TEnum">The enum which should be added to the cache.</typeparam>
 		public CacheInitializer InitWith<TEnum>() where TEnum : struct, Enum
 		{
-			var enumType = typeof(TEnum);
+			InitWith(typeof(TEnum));
+
+			return this;
+		}
+
+		/// <summary>
+		/// Initializes the cache with a set of given enums.
+		/// </summary>
+		/// <param name="enums">The enums which should be added to the cache.</param>
+		public CacheInitializer InitWith(params Enum[] enums)
+		{
+			foreach (var enumVal in enums)
+			{
+				InitWith(enumVal);
+			}
+
+			return this;
+		}
+
+		private void InitWith(Type enumType)
+		{
+			if (!enumType.IsEnum)
+				throw new ArgumentException("The type provided is not of type enum", nameof(enumType));
 
 			var fields = enumType.GetFields();
 
@@ -77,8 +86,6 @@ namespace StringyEnums
 			{
 				_tempCache.Add(enumType, temptDict);
 			}
-
-			return this;
 		}
 
 		internal IReadOnlyDictionary<Type, IReadOnlyBiDictionary<int, string>> CustructCache()
