4075eb352a934475e4a6e6d421290d29995ade83
Twenty <twenty@translucent.at>
2020-02-24 00:16:47 +0100

    XML Documentation

--- src/StringyEnums/StringyEnums/StringRepresentationAttribute.cs
+++ src/StringyEnums/StringyEnums/StringRepresentationAttribute.cs
@@ -2,11 +2,18 @@
 
 namespace StringyEnums
 {
+	/// <summary>
+	/// Defines the string representation for an enum member.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
 	public sealed class StringRepresentationAttribute : Attribute
 	{
 		internal readonly string StringRepresentation;
 
+		/// <summary>
+		/// Sets the string representation for an enum member.
+		/// </summary>
+		/// <param name="stringRepresentation">The string representation which should be set-</param>
 		public StringRepresentationAttribute(string stringRepresentation)
 		{
 			StringRepresentation = stringRepresentation;
